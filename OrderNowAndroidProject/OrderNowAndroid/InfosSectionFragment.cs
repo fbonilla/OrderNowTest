29cf2a5aaf3e0b0849182a10221d44fce75d8e78
Fredy Alexander <fabonill12@gmail.com>
2015-04-03 23:16:55 -0400

    Ajout du projet complet
    ajout du projet complet

---
+++ OrderNowAndroidProject/OrderNowAndroid/InfosSectionFragment.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Android.App;
+using Android.Views;
+using Android.OS;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+
+namespace OrderNowAndroid
+{
+	public class InfosSectionFragment : Fragment,IOnMapReadyCallback
+	{
+		private GoogleMap mMap;
+
+
+		public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+			base.OnCreateView (inflater, container, savedInstanceState);
+
+			var view = inflater.Inflate(Resource.Layout.infos_section, container, false);
+
+			//Set up google maps coords in infos section
+			SetUpMap ();
+
+			return view;
+		}
+
+		private void SetUpMap()
+		{
+			if (mMap == null) 
+			{
+				FragmentManager.FindFragmentById<MapFragment>(Resource.Id.map).GetMapAsync(this);
+			}
+		}
+
+		//Launching of the actual map
+		public void OnMapReady (GoogleMap googleMap)
+		{
+			// Clean memory to prevent crashing from out of memory
+			System.GC.Collect ();
+
+			mMap = googleMap;
+			mMap.MapType = GoogleMap.MapTypeNormal;
+
+			//Instances of latitutes and longitudes
+			LatLng downtownlatlng = new LatLng (45.505616,-73.569678);
+			LatLng gayLatLng = new LatLng (45.516428, -73.558173);
+			LatLng jewLatLng = new LatLng (45.493632, -73.581836);
+
+			//Default position of the camera on the map, the chosen location is montreal downtown
+			CameraUpdate camera = CameraUpdateFactory.NewLatLngZoom(downtownlatlng, 12);
+			mMap.MoveCamera (camera);
+
+			//Creation of the marker from the first position
+			MarkerOptions gayStationOptions = new MarkerOptions ();
+			gayStationOptions.SetPosition(gayLatLng);
+
+			//Creation of the marker frome the second position
+			MarkerOptions jewStationOptions = new MarkerOptions ();
+			jewStationOptions.SetPosition(jewLatLng);
+
+			//Adding the markers on the map
+			mMap.AddMarker (gayStationOptions);
+			mMap.AddMarker (jewStationOptions);
+
+			mMap.CameraChange += delegate(object sender, GoogleMap.CameraChangeEventArgs e)
+			{
+				// Navigation cleaning to prevent out of memory
+				System.GC.Collect();
+			};
+		}
+	}
+
+}
---
+++ OrderNowAndroidProject/OrderNowAndroid/obj/Debug/android/src/ordernowandroid/InfosSectionFragment.java
@@ -0,0 +1,56 @@
+package ordernowandroid;
+
+
+public class InfosSectionFragment
+	extends android.app.Fragment
+	implements
+		mono.android.IGCUserPeer,
+		com.google.android.gms.maps.OnMapReadyCallback
+{
+	static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreateView:(Landroid/view/LayoutInflater;Landroid/view/ViewGroup;Landroid/os/Bundle;)Landroid/view/View;:GetOnCreateView_Landroid_view_LayoutInflater_Landroid_view_ViewGroup_Landroid_os_Bundle_Handler\n" +
+			"n_onMapReady:(Lcom/google/android/gms/maps/GoogleMap;)V:GetOnMapReady_Lcom_google_android_gms_maps_GoogleMap_Handler:Android.Gms.Maps.IOnMapReadyCallbackInvoker, GooglePlayServicesLib\n" +
+			"";
+		mono.android.Runtime.register ("OrderNowAndroid.InfosSectionFragment, OrderNowAndroid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", InfosSectionFragment.class, __md_methods);
+	}
+
+
+	public InfosSectionFragment () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == InfosSectionFragment.class)
+			mono.android.TypeManager.Activate ("OrderNowAndroid.InfosSectionFragment, OrderNowAndroid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public android.view.View onCreateView (android.view.LayoutInflater p0, android.view.ViewGroup p1, android.os.Bundle p2)
+	{
+		return n_onCreateView (p0, p1, p2);
+	}
+
+	private native android.view.View n_onCreateView (android.view.LayoutInflater p0, android.view.ViewGroup p1, android.os.Bundle p2);
+
+
+	public void onMapReady (com.google.android.gms.maps.GoogleMap p0)
+	{
+		n_onMapReady (p0);
+	}
+
+	private native void n_onMapReady (com.google.android.gms.maps.GoogleMap p0);
+
+	java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
